de995f014e5954a40632447ad030f1937dbefedb
Luciana Heuko <lucianaheuko@Lucianas-MBP.home>
2015-12-16 13:31:49 -0200

    header style

--- src/views/header/controller.js
+++ src/views/header/controller.js
@@ -1,10 +1,28 @@
 'use strict';
 
+
 module.exports = /*@ngInject*/ function HeaderCtrl($scope, $stateParams, $state, auth) {
+  
+  setTimeout( function() {
+    console.log($scope.currentUser);  
+  }, 1000);
+  
+  
+  $scope.menuIsOpen = false;
+  
+  $scope.toggleMenu = function () {
+    if ($scope.menuIsOpen) {
+      $scope.menuIsOpen = false;
+    } else {
+      $scope.menuIsOpen = true; 
+    }
+  }
+  
   $scope.logOut = function () {
     auth.logOut()
       .then(function () {
         $state.go('login');
       });
   };
+  
 };
