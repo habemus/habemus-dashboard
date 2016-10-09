4784cdce0c8fc637813f19a41f0f4da0114032de
Simon <simonxd@gmail.com>
2016-10-09 03:38:23 -0300

    updated h-workspace-client

--- src/services/api/h-workspace.js
+++ src/services/api/h-workspace.js
@@ -1,9 +1,6 @@
-const HWorkspaceClient = require('h-workspace-client/authenticated');
+const HWorkspaceClient = require('h-workspace-client/public/authenticated');
 
 module.exports = /* @ngInject */ function (CONFIG) {
-
-  console.log(CONFIG)
-
   return new HWorkspaceClient({
     apiVersion: '0.0.0',
     serverURI: CONFIG.hWorkspaceURI,
