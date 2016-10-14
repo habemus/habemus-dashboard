3b3f6eee25122ec8dcda01b11f9973b29ada1d7d
Simon <simonxd@gmail.com>
2016-10-14 03:09:58 -0300

    not displaying url until the project latest version has its buildStatus at succeeded

--- src/views/project/controller.js
+++ src/views/project/controller.js
@@ -7,6 +7,7 @@ var url  = require('url');
 
 // third-party
 var Q    = require('q');
+const Bluebird = require('bluebird');
 
 const STARTING_RE = /^\//;
 const TRAILING_RE = /\/$/;
@@ -15,19 +16,28 @@ function _joinPath(part1, part2) {
   return part1.replace(TRAILING_RE, '') + '/' + part2.replace(STARTING_RE, '');
 }
 
-module.exports = /*@ngInject*/ function ProjectCtrl($scope, $stateParams, $rootScope, $translate, apiHProject, apiHWorkspace, uiHAccountDialog, ngDialog, CONFIG, uiDialogLoading, auxZipPrepare, auxZipUpload, uiIntro) {
+function _wait(ms) {
+  return new Bluebird(function (resolve, reject) {
+    setTimeout(resolve, ms);
+  });
+}
 
+module.exports = /*@ngInject*/ function ProjectCtrl($scope, $stateParams, $rootScope, $translate, apiHProject, apiHWorkspace, uiHAccountDialog, ngDialog, CONFIG, uiDialogLoading, auxZipPrepare, auxZipUpload, uiIntro) {
   
   uiHAccountDialog.ensureUser({ ensureEmailVerified: true })
     .then(function (user) {
       return $scope.loadProject();
     })
     .then(function (project) {
-      return $scope.ensureWorkspaceReady();
+      return Bluebird.all([
+        $scope.ensureLatesteVersionBuildReady(),
+        $scope.ensureWorkspaceReady(),
+      ]);
     });
 
   /**
-   * Project data loading
+   * Loads the project's data into the scope
+   * @return {Bluebird -> Project}
    */
   $scope.loadProject = function () {
     // retrieve the requested project
@@ -42,10 +52,6 @@ module.exports = /*@ngInject*/ function ProjectCtrl($scope, $stateParams, $rootS
 
       $scope.project = project;
 
-      // var parsedHostURL = url.parse(CONFIG.hostURL);
-      // parsedHostURL.host = project.code + '.' + parsedHostURL.host;
-      // $scope.projectHostURL = url.format(parsedHostURL);
-
       // set pageTitle
       $rootScope.pageTitle = project.name;
 
@@ -55,6 +61,65 @@ module.exports = /*@ngInject*/ function ProjectCtrl($scope, $stateParams, $rootS
     });
   };
 
+  /**
+   * Calls `loadProjectLatestVersion` until
+   * the latestVersion's buildStatus is at either `failed` or `succeeded`
+   * @return {Bluebird -> ProjectVersion}
+   */
+  $scope.ensureLatesteVersionBuildReady = function () {
+
+    return $scope.loadProjectLatestVersion()
+      .then(function (projectLatestVersion) {
+
+        switch (projectLatestVersion.buildStatus.value) {
+          case 'failed':
+            return Bluebird.reject(new Error('build failed'));
+            break;
+          case 'succeeded':
+            return projectLatestVersion;
+            break;
+          case 'not-scheduled':
+          case 'scheduled':
+          case 'started':
+            return _wait(3000).then(function () {
+              return $scope.ensureLatesteVersionBuildReady();
+            });
+          default:
+            return Bluebird.reject(new Error('invalid build status, unknown error'));
+            break;
+        }
+      })
+  };
+
+  /**
+   * Loads the project's latest version into the scope.
+   * 
+   * @return {Bluebird -> ProjectVersion}
+   */
+  $scope.loadProjectLatestVersion = function () {
+    return apiHProject.getLatestVersion(
+      uiHAccountDialog.getAuthToken(),
+      $stateParams.projectCode,
+      {
+        byCode: true
+      }
+    )
+    .then(function (projectLatestVersion) {
+
+      $scope.projectLatestVersion = projectLatestVersion;
+      $scope.$apply();
+
+      return projectLatestVersion;
+    });
+  };
+
+  /**
+   * Creates the workspace related to this project
+   * only if it does not exist.
+   * Loads the workspace's data into the scope.
+   * 
+   * @return {Bluebird -> Workspace}
+   */
   $scope.ensureWorkspaceReady = function () {
 
     return $scope.loadProject()
