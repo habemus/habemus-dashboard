a06935f7df5fe66a9963a66cd7600a3f04ef8fb3
Simon <simonxd@gmail.com>
2016-10-15 19:36:13 -0300

    using h-urls for address generation

--- src/views/project-domain/dns/controller.js
+++ src/views/project-domain/dns/controller.js
@@ -7,19 +7,65 @@ module.exports = /*@ngInject*/ function tabCtrlDomainDns ($scope, $stateParams, 
   /**
    * Var to hold reference to the interval that checks for domain status
    */
-  var CHECK_INTERVAL
-
-  /**
-   * Whether the connection is still in progress
-   * @type {[type]}
-   */
-  $scope.inProgress = $stateParams.inProgress;
-
+  var CHECK_INTERVAL;
+  
   /**
    * List of targetDNSRecords and their respective data and statuses
    * @type {Array}
    */
   $scope.targetDNSRecords = [];
+
+  /**
+   * Set targetDNSRecords according to data from the domainRecord
+   */
+  $scope.$watch('domainRecord', function () {
+
+    var domainRecord = $scope.domainRecord;
+    var targetDNSRecords = [];
+
+    // verification code TXT
+    var _txtPartials = domainRecord.verification.computedPartialResults.txt || {
+      active: false,
+      score: 0,
+    };
+    targetDNSRecords.push({
+      type: 'TXT',
+      host: domainRecord.verification.subdomain,
+      targetValue: domainRecord.verification.code,
+      active: _txtPartials.active,
+      score: _txtPartials.score,
+    });
+
+    domainRecord.ipAddresses.forEach((ipAddress) => {
+      var _ipv4Partials = domainRecord.verification.computedPartialResults.ipv4 || {
+        active: false,
+        score: 0,
+      }
+      targetDNSRecords.push({
+        type: 'A',
+        host: '@',
+        targetValue: ipAddress,
+        active: _ipv4Partials.active,
+        score: _ipv4Partials.score,
+      });
+    });
+
+    if (domainRecord.enableWwwAlias) {
+      var _cnamePartials = domainRecord.verification.computedPartialResults.cname || {
+        active: false,
+        score: 0
+      };
+      targetDNSRecords.push({
+        type: 'CNAME',
+        host: 'www',
+        targetValue: domainRecord.domain,
+        active: _cnamePartials.active,
+        score: _cnamePartials.score,
+      });
+    }
+
+    $scope.targetDNSRecords = targetDNSRecords;
+  })
 
   $scope.domainRecord = $stateParams.domainRecord;
 
@@ -31,66 +77,50 @@ module.exports = /*@ngInject*/ function tabCtrlDomainDns ($scope, $stateParams, 
     
     $scope.verifying = true;
     
-    apiHWebsite
+    return apiHWebsite
       .getDomainRecord(
         uiHAccountDialog.getAuthToken(),
         $scope.domainRecord.projectId,
         $stateParams.domainRecord._id
       )
       .then(function (domainRecord) {
-
-        $scope.targetDNSRecords = [];
-
-        // verification code TXT
-        var _txtPartials = domainRecord.verification.computedPartialResults.txt || {
-          active: false,
-          score: 0,
-        };
-        $scope.targetDNSRecords.push({
-          type: 'TXT',
-          host: domainRecord.verification.subdomain,
-          targetValue: domainRecord.verification.code,
-          active: _txtPartials.active,
-          score: _txtPartials.score,
-        });
-
-        domainRecord.ipAddresses.forEach((ipAddress) => {
-          var _ipv4Partials = domainRecord.verification.computedPartialResults.ipv4 || {
-            active: false,
-            score: 0,
-          }
-          $scope.targetDNSRecords.push({
-            type: 'A',
-            host: '@',
-            targetValue: ipAddress,
-            active: _ipv4Partials.active,
-            score: _ipv4Partials.score,
-          });
-        });
-
-        if (domainRecord.enableWwwAlias) {
-          var _cnamePartials = domainRecord.verification.computedPartialResults.cname || {
-            active: false,
-            score: 0
-          };
-          $scope.targetDNSRecords.push({
-            type: 'CNAME',
-            host: 'www',
-            targetValue: domainRecord.domain,
-            active: _cnamePartials.active,
-            score: _cnamePartials.score,
-          });
-        }
-
         $scope.verifying = false;
+        $scope.domainRecord = domainRecord;
         $scope.$apply();
 
       }, function (err) {
         console.warn(err);
 
         uiDialogLoading.close();
+      });
+  };
+
+  /**
+   * Restarts the verification process for the domainRecord
+   * @return {Bluebird -> DomainRecord}
+   */
+  $scope.restartVerification = function () {
+
+    uiDialogLoading.open({
+      message: 'scheduling domain verification restart'
+    });
+
+    return apiHWebsite
+      .restartDomainRecordVerification(
+        uiHAccountDialog.getAuthToken(),
+        $scope.domainRecord.projectId,
+        $stateParams.domainRecord._id
+      )
+      .then(function (domainRecord) {
+        $scope.domainRecord = domainRecord;
+        $scope.$apply();
+
+        uiDialogLoading.close();
       })
-      .done();
+      .catch(function (err) {
+        console.warn(err);
+        uiDialogLoading.close();
+      });
   };
 
   // start
