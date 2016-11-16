9d34fb5ac3ff43f466c9489aae329d7daea8bbf0
Simon <simonxd@gmail.com>
2016-11-15 22:33:58 -0200

    lang parameter for workspace url

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,3 +1,6 @@
+// native
+const url = require('url');
+
 module.exports = function (DASHBOARD) {
   DASHBOARD.filter('urlWebsiteHabemusDomain', function(auxUrls) {
     return function(projectCode, versionCode) {
@@ -11,9 +14,22 @@ module.exports = function (DASHBOARD) {
     };
   });
 
-  DASHBOARD.filter('urlWorkspace', function (auxUrls) {
+  DASHBOARD.filter('urlWorkspace', function (auxUrls, $translate) {
     return function (projectCode) {
-      return projectCode ? auxUrls.format.uiWorkspace(projectCode) : '';
+      if (!projectCode) {
+        return '';
+      } else {
+
+        var uiWorkspaceURL = auxUrls.format.uiWorkspace(projectCode);
+
+        // add the language query parameter:
+        var parsed = url.parse(uiWorkspaceURL, true);
+        // force url.format to use query object instead of the search
+        delete parsed.search;
+        parsed.query.lang = $translate.use();
+
+        return url.format(parsed);
+      }
     }
   });
 };
